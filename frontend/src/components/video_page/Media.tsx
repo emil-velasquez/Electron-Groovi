bdb44c554d09fd4b11c231caa38d30a2cbaeee29
Emil Velasquez <evelas10@utexas.edu>
2022-07-12 23:52:09 -0500

    Users can define a focus area

--- frontend/src/components/video_page/Media.tsx
+++ frontend/src/components/video_page/Media.tsx
@@ -1,6 +1,6 @@
 import "../../styles/video_page/MediaStyle.css"
 
-import React, { useRef, useState, useImperativeHandle, forwardRef, Ref } from "react";
+import React, { useRef, useState, useImperativeHandle, forwardRef, Ref, useEffect } from "react";
 
 import Webcam from "react-webcam";
 import { Pose, POSE_CONNECTIONS } from "@mediapipe/pose";
@@ -8,18 +8,20 @@ import * as mp_drawing from "@mediapipe/drawing_utils";
 
 //TODO: make sure to update props when we figure out what props to enter
 function Media(props: any, ref: Ref<unknown>) {
-    const JOINTS: number[][] = [[16, 14, 12], [14, 12, 11], [14, 12, 24], [12, 24, 26], [24, 26, 28],
+    const JOINTS = [[16, 14, 12], [14, 12, 11], [14, 12, 24], [12, 24, 26], [24, 26, 28],
     [26, 24, 23], [25, 23, 24], [23, 25, 27], [11, 23, 25], [13, 11, 23],
     [13, 11, 12], [15, 13, 11]];
+
+    const HALF_HIDDEN_VIDEO_WIDTH = 0.22;
 
     const videoRef = useRef<HTMLVideoElement>(null);
     const videoFocusSelectionCanvasRef = useRef<HTMLCanvasElement>(null);
 
-    const videoFocusCanvas = useRef<HTMLCanvasElement>(null);
+    const videoFocusCanvasRef = useRef<HTMLCanvasElement>(null);
     const videoPoseCanvasRef = useRef<HTMLCanvasElement>(null);
 
     const mouseDown = useRef(false);
-    const focusInterval = useRef<number>(null);
+    const focusInterval = useRef<number | null>(null);
     const [rect, setRect] = useState({ startX: 0, startY: 0, width: 0, height: 0, updatedRect: false });
 
 
@@ -155,16 +157,176 @@ function Media(props: any, ref: Ref<unknown>) {
         mouseDown.current = false;
     }
 
+    /**
+    * Returns a new pose model WITHOUT its onResults set
+    * pre: none
+    */
+    const getPoseModel = () => {
+        let pose = new Pose({
+            locateFile: (file) => {
+                return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+            },
+        });
+        pose.setOptions({
+            modelComplexity: 1,
+            smoothLandmarks: true,
+            minDetectionConfidence: 0.5,
+            minTrackingConfidence: 0.5,
+        });
+        return pose;
+    }
+
+    /**
+     * Performs the actual drawing of the pose model to the screen
+     * Accepts either a video element or a canvas element
+     */
+    const startPoseEstimation = (poseModel: Pose, mediaElement: HTMLCanvasElement | HTMLVideoElement) => {
+        const poseDetectionFrame = async () => {
+            await poseModel.send({ image: mediaElement });
+            requestAnimationFrame(poseDetectionFrame);
+        }
+        poseDetectionFrame();
+    }
+
+    /**
+     * Draws the results of the pose model on the passed in canvas
+     */
+    const drawResults = (results: any, canvas: HTMLCanvasElement) => {
+        if (!results.poseLandmarks || canvas === null || canvas === undefined) {
+            return;
+        }
+
+        const canvasCtx = canvas.getContext("2d");
+        if (canvasCtx !== null) {
+            //ready the canvas
+            canvasCtx.save();
+            canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+            //drawing to the canvas
+            canvasCtx.globalCompositeOperation = "source-over";
+            mp_drawing.drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+                { color: "#00FF00", lineWidth: 2 });
+            mp_drawing.drawLandmarks(canvasCtx, results.poseLandmarks, { color: "#0000FF", lineWidth: 0.5 });
+            canvasCtx.restore();
+        }
+    }
+
+    /**
+     * When the video is loaded, set the focus area to the 50% actually shown to the user
+     * Also start the pose model
+     * pre: video is loaded
+     */
+    const initVideoCanvas = () => {
+        const video = videoRef.current;
+        const videoFocusSelectionCanvas = videoFocusSelectionCanvasRef.current;
+        if (videoFocusSelectionCanvas !== null && video !== null) {
+            const bounds = videoFocusSelectionCanvas.getBoundingClientRect();
+            setRect(prevRect => ({
+                startX: 0,
+                startY: 0,
+                width: HALF_HIDDEN_VIDEO_WIDTH * video.videoWidth,
+                height: video.videoHeight / bounds.height * videoFocusSelectionCanvas.height,
+                updatedRect: true
+            }))
+        }
+
+        const videoCanvasPoseModel = getPoseModel();
+        videoCanvasPoseModel.onResults(videoOnResults)
+
+        if (videoFocusCanvasRef.current !== null) {
+            startPoseEstimation(videoCanvasPoseModel, videoFocusCanvasRef.current)
+        }
+    }
+
+    /**
+     * Handle whenever the video pose model makes results
+     */
+    const videoOnResults = (results: any) => {
+        if (videoPoseCanvasRef.current !== null) {
+            console.log("video results")
+            console.log(results.poseLandmarks);
+            drawResults(results, videoPoseCanvasRef.current);
+        }
+    }
+
+    /**
+     * Make the focus area canvas start drawing the updated focus area rectangle
+     */
+    useEffect(() => {
+        if (rect.updatedRect) {
+            const drawVideoDance = () => {
+                const videoFocusSelectionCanvas = videoFocusSelectionCanvasRef.current;
+                if (videoFocusSelectionCanvas !== null) {
+                    const bounds = videoFocusSelectionCanvas.getBoundingClientRect();
+                    const scaleX = bounds.width / videoFocusSelectionCanvas.width;
+                    const scaleY = bounds.height / videoFocusSelectionCanvas.height;
+
+                    adjustFocusAndPoseCanvas(scaleX, scaleY);
+
+                    const videoFocusCanvas = videoFocusCanvasRef.current;
+                    const video = videoRef.current;
+                    if (videoFocusCanvas !== null && video != null) {
+                        const focusCanvasContext = videoFocusCanvas.getContext("2d");
+
+                        let focusX = rect.startX * scaleX + video.videoWidth * .22;
+                        let focusY = rect.startY * scaleY;
+                        const focusWidth = rect.width * scaleX;
+                        const focusHeight = rect.height * scaleY;
+
+                        if (focusX < video.videoWidth * .5) {
+                            let topRightX = focusX + focusWidth;
+                            let distance = video.videoWidth * .5 - topRightX;
+                            focusX = focusX + 2 * distance + focusWidth;
+                        } else {
+                            let distance = focusX - video.videoWidth * .5;
+                            focusX = focusX - 2 * distance - focusWidth;
+                        }
+                        if (focusCanvasContext !== null) {
+                            focusCanvasContext.drawImage(video, focusX, focusY, focusWidth, focusHeight, 0, 0,
+                                videoFocusCanvas.width, videoFocusCanvas.height);
+                        }
+                    }
+                }
+
+            }
+
+            //resizes the focus area and pose canvas according to the rectangle
+            const adjustFocusAndPoseCanvas = (scaleX: number, scaleY: number) => {
+                const focusCanvas = videoFocusCanvasRef.current;
+                if (focusCanvas !== null) {
+                    focusCanvas.width = rect.width * scaleX;
+                    focusCanvas.height = rect.height * scaleY;
+                }
+
+                const poseCanvas = videoPoseCanvasRef.current;
+                if (poseCanvas !== null) {
+                    poseCanvas.width = rect.width * scaleX;
+                    poseCanvas.height = rect.height * scaleY;
+                }
+            }
+
+            if (focusInterval.current !== null) {
+                window.clearInterval(focusInterval.current);
+            }
+
+            focusInterval.current = window.setInterval(() => {
+                drawVideoDance();
+            }, 50);
+
+            setRect(prevRect => ({ ...prevRect, updatedRect: false }))
+        }
+    }, [rect])
+
     return (
         <div className="media-container">
             <div className="video-container">
-                <video crossOrigin="Anonymous" className="video-element" ref={videoRef}>
+                <video crossOrigin="Anonymous" className="video-element" ref={videoRef} onLoadedData={initVideoCanvas}>
                     <source src="./videos/BTBT.mp4" type="video/mp4" />
                 </video>
             </div>
             <canvas className="video-focus-selection-canvas" ref={videoFocusSelectionCanvasRef}
                 onMouseDown={setRectStart} onMouseMove={dragRect} onMouseUp={finalizeRect} />
-            <canvas className="focus-area" ref={videoFocusCanvas} />
+            <canvas className="focus-area" ref={videoFocusCanvasRef} />
             <canvas className="focus-area-pose" ref={videoPoseCanvasRef} />
 
             <Webcam className="camera-elements" />
