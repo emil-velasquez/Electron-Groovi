0e085caed7d7dd4c81d42ee9b520cc794bd4b903
Emil Velasquez <evelas10@utexas.edu>
2022-07-28 17:06:06 -0500

    Templated out the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,12 +11,33 @@ function App() {
 
     return (
         <div className="App">
-            <nav>
-                <Link to="/user">User</Link> | {" "}
-                <Link to="/playlist">Playlist</Link> | {" "}
-                <Link to="/song">Song</Link>
-            </nav>
-            <Outlet />
+            <div className="top-bar">
+                <div className="top-bar-section">
+                    <p>Logo</p>
+                </div>
+                <div className="top-bar-section search-elements">
+                    <p>Search Bar</p>
+                    <p>Submit Search</p>
+                </div>
+                <div className="top-bar-section right-elements">
+                    <p>Profile</p>
+                    <p>Settings</p>
+                </div>
+            </div>
+            <div className="side-bar">
+                <Link className="side-bar-link" to="/playlist">
+                    <p>Home</p>
+                </Link>
+                <p>Trending</p>
+                <p>Liked</p>
+                <hr />
+                <p>Playlists</p>
+                <hr />
+                <p>Subscribed</p>
+            </div>
+            <div className="outlet-section">
+                <Outlet />
+            </div>
         </div>
     );
 }
