c61ef9d1ea0c18aed2a40173ef713f723e8652ea
Emil Velasquez <evelas10@utexas.edu>
2023-01-08 13:07:05 -0600

    Rating now given on score update

--- frontend/src/components/video_learn_page/Score.tsx
+++ frontend/src/components/video_learn_page/Score.tsx
@@ -1,13 +1,40 @@
 import "../../styles/video_learn_page/ScoreStyle.css"
 
+import React, { useEffect, useState } from "react"
+
 type ScoreProps = {
     score: number
 };
 
 function Score(props: ScoreProps) {
+    const [scoreClass, setScoreClass] = useState("perfect-score");
+    const [scoreText, setScoreText] = useState("PERFECT");
+
+    useEffect(() => {
+        if (props.score <= 500) {
+            setScoreClass("perfect-score");
+            setScoreText("PERFECT");
+        } else if (props.score <= 600) {
+            setScoreClass("great-score");
+            setScoreText("GREAT");
+        } else if (props.score <= 700) {
+            setScoreClass("good-score");
+            setScoreText("Good");
+        } else if (props.score <= 800) {
+            setScoreClass("ok-score");
+            setScoreText("ok");
+        } else if (props.score <= 2000) {
+            setScoreClass("miss-score");
+            setScoreText("x");
+        } else {
+            setScoreClass("");
+            setScoreText("");
+        }
+    }, [props.score])
+
     return (
         <div>
-            <p className="score">{props.score}</p>
+            <p className={`score ${scoreClass}`}>{scoreText}</p>
         </div>
     );
 }
